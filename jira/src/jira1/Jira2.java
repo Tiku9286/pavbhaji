b26f3440357311863f67e72c5a32a31c8b7183bf
rahul <rahul@TIKUDELL-2011>
2023-03-07 14:10:17 +0530

    xxx

---
+++ jira/src/jira1/Jira2.java
@@ -0,0 +1,14 @@
+package jira1;
+
+public class Jira2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		System.out.println("amey");
+
+
+	}
+
+}
